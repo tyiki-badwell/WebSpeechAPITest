3f0c7b9630b1c126ffc85f85444b5c3824c8b2be
tyiki badwell <tyiki.badwell@gmail.com>
2023-06-12 01:53:55 +0900

    .

--- api/src/functions/translate.js
+++ api/src/functions/translate.js
@@ -1,38 +1,44 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const { app } = require('@azure/functions');
 
-    const api = 'https://api-free.deepl.com/v2/translate';
-    try {
-            const params = new URLSearchParams();
-            params.append('text', req.value);
-            params.append('source_lang', 'JA');
-            params.append('target_lang', 'EN');
+app.http('translate', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const api = 'https://api-free.deepl.com/v2/translate';
+        try {
+                const params = new URLSearchParams();
+                params.append('text', req.value);
+                params.append('source_lang', 'JA');
+                params.append('target_lang', 'EN');
+        
+                const response = await fetch(api, {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Authorization': `DeepL-Auth-Key ${process.env["DEEPL_API_KEY"]}`
+                },
+                body: params
+            });
     
-            const response = await fetch(api, {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Authorization': `DeepL-Auth-Key ${process.env["DEEPL_API_KEY"]}`
-            },
-            body: params
-        });
-
-        if (response.ok) {
-            const json = await response.json();
-            context.log(`${json}`);
-
-            context.res.json(json);
-        } else {
-            const json = await response.json();
-            context.log(`${json}`);
-
-            context.res.json(json);
+            if (response.ok) {
+                const json = await response.json();
+                context.log(`${json}`);
+    
+                context.res.json(json);
+            } else {
+                const json = await response.json();
+                context.log(`${json}`);
+    
+                context.res.json(json);
+            }
+        } catch(e) {
+            context.log(`${e}`);
+     
+            context.res.json({
+                error: e
+            });
         }
-    } catch(e) {
-        context.log(`${e}`);
- 
-        context.res.json({
-            error: e
-        });
     }
-}
+});
